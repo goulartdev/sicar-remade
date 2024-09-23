1807d65d55edcc4b7a2d6d26ceb7a444cc51909e
Djonathan Goulart <contact@djonathan.dev>
2024-09-22 23:39:41 -0300

    setup map with sattelite basemap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,25 @@
 import { TuiRoot } from "@taiga-ui/core";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import {
-  LayerComponent,
-  MapComponent,
-  RasterSourceComponent,
-  VectorSourceComponent,
-} from "@maplibre/ngx-maplibre-gl";
-import { LngLatBoundsLike, StyleSpecification, addProtocol } from "maplibre-gl";
-import { Protocol } from "pmtiles";
+import { LngLatBoundsLike, StyleSpecification } from "maplibre-gl";
 
-let protocol = new Protocol();
-addProtocol("pmtiles", protocol.tile);
+import { MapComponent } from "./components/map/map.component";
+import { MapService } from "@maplibre/ngx-maplibre-gl";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    RouterOutlet,
-    TuiRoot,
-    MapComponent,
-    VectorSourceComponent,
-    RasterSourceComponent,
-    LayerComponent,
-  ],
+  imports: [RouterOutlet, TuiRoot, MapComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
   changeDetection: ChangeDetectionStrategy.OnPush,
+
+  providers: [MapService],
 })
 export class AppComponent {
-  public readonly attributionControl = false;
-  public bounds: LngLatBoundsLike = [-33.437108, 6.672897, -74.404622, -34.796086];
-  public readonly style: StyleSpecification = {
+  protected layersVisible = false;
+  protected bounds: LngLatBoundsLike = [-33.437108, 6.672897, -74.404622, -34.796086];
+  protected readonly style: StyleSpecification = {
     version: 8,
     sources: {
       satellite: {
@@ -41,14 +29,6 @@ export class AppComponent {
         ],
         tileSize: 256,
       },
-      area_imovel: {
-        type: "vector",
-        url: "pmtiles://http://localhost:9000/sicar/AREA_IMOVEL_2.pmtiles",
-      },
-      admin_boundaries: {
-        type: "vector",
-        url: "pmtiles://http://localhost:9000/sicar/admin_boundaries.pmtiles",
-      },
     },
     layers: [
       {
@@ -57,45 +37,6 @@ export class AppComponent {
         type: "raster",
         layout: {
           visibility: "visible",
-        },
-      },
-      {
-        id: "area_imovel",
-        source: "area_imovel",
-        "source-layer": "AREA_IMOVEL",
-        type: "fill",
-        layout: {
-          visibility: "visible",
-        },
-        paint: {
-          "fill-color": [
-            "match",
-            ["get", "ind_status"],
-            "AT",
-            "#4567ff",
-            "PE",
-            "#ffc533",
-            "SU",
-            "#e0351b",
-            "CA",
-            "#7d7d7d",
-            "#000000",
-          ],
-          "fill-outline-color": "#000000",
-          "fill-opacity": 0.5,
-        },
-      },
-      {
-        id: "admin_boundaries",
-        source: "admin_boundaries",
-        "source-layer": "admin_boundaries",
-        type: "line",
-        layout: {
-          visibility: "visible",
-        },
-        paint: {
-          "line-color": "#d3650a",
-          "line-width": 2,
         },
       },
     ],
